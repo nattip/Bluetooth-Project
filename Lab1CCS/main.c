f37a0f596e9e8692ce9dee773cf433b227a81c37
Natalie Tipton <43283610+nattip@users.noreply.github.com>
2019-02-03 15:18:25 -0500

    Add files via upload

--- Lab1CCS/main.c
+++ Lab1CCS/main.c
@@ -1,65 +1,59 @@
-#include "msp.h"
-#include <stdio.h>
-#include <timersinit.h>
+/****************************************
+ * Written by Natalie Tipton and Andrew Deradoorian
+ * EGR 436 (102)
+ * Dr. Brakora
+ * Lab 1
+ * 1/7/19
+ * This code uses interrupts to flash an onboard red
+ *      LED at specific time intervals. These intervals
+ *      are dependent on data that is read in from the
+ *      computer over UART.
+ ****************************************/
 
-/**
- * main.c
- */
-void UART0_init(void);
-uint16_t changedSpeed=60;
+#include "msp.h"    //include necessary libraries
+#include <stdio.h>
+#include <timers.h>
+#include <IO.h>
+#include <UART.h>
+
+uint16_t changedSpeed=60;   //speed of flashing LED
+
+
 void main(void)
 {
-    __disable_irq();
+    __disable_irq();    //disable interrupts to start
+
+    //initialize variables
     uint16_t TopValue=4096;
     uint16_t basespeed=60;
     uint16_t ratio;
-
     uint16_t AlteredCount;
+    char readData;
 
-	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
-	P2->SEL1 &=~1;
-	P2->SEL0 &=~1;
-	P2->DIR |=1;
+    STOP_WDT;   //stop watch dog timer
 
-	NVIC_SetPriority(TA1_0_IRQn,3);
-	NVIC_EnableIRQ(TA1_0_IRQn);
+    NVIC_SetPriority(TA1_0_IRQn,3);     //set priority of interrupt
+    NVIC_EnableIRQ(TA1_0_IRQn);         //enable interrupt
 
-	UART0_init();
-	timerA_Init();
+    UART0_Init();   //initialize UART0
+    timerA_Init();  //initialize TimerA
+    onboardLED_setup(); //setup onboard LED
 
-	while(1)
+    //continuous while loop
+    while(1){
 
-{
-	   ratio=(100*basespeed)/changedSpeed;
-	   AlteredCount=(TopValue*ratio)/100;
-	   TIMER_A1->CCR[0]=AlteredCount/2;
+       ratio=(100*basespeed)/changedSpeed; //math for the ratio between basespeed and the altered speed
+       AlteredCount=(TopValue*ratio)/100; //altering the top count value to be scaled to correct bpm
+       TIMER_A1->CCR[0]=AlteredCount/2; //setting top count value in register
 
-//    while((TIMER_A1->CCTL[0] & 1)==0);
-  //      TIMER_A1->CCTL[0] &=~1;
-    //    P2->OUT ^=2;
+       readData = UART0_Read(); //reads data from UART
 
-}
-}
+       if(readData == 'u') changedSpeed += 2;   //if a 'u' is read in from UART, increase speed by 2 bpm
+       else if(readData == 'd') changedSpeed -= 2;  //if a 'd' is read in from UART, decrease speed by 2 bmp
+       else if(readData == 'r') changedSpeed = 60;  //if an 'r' is read in from UART, reset speed to 60 bpm
 
-void TA1_0_IRQHandler (void)
-{
-    TIMER_A1->CCTL[0] &=~1;
-    P2->OUT ^=1;
-
-    while(!(EUSCI_A0->IFG & 0x02)) {}
-    EUSCI_A0->TXBUF=changedSpeed;
-    while(!(EUSCI_A0->IFG & 0x02)) {}
-    EUSCI_A0->TXBUF='\n';
+       UART0_Write(changedSpeed);   //writes current speed over UART to the computer
+    }
 }
 
-void UART0_init(void)
-{
-    EUSCI_A0->CTLW0 |=1;
-    EUSCI_A0->MCTLW=0;
-    EUSCI_A0->CTLW0=0x0081;
-    EUSCI_A0->BRW=26;
-    P1->SEL0 |=0x0C;
-    P1->SEL1 &=~ 0x0C;
-    EUSCI_A0->CTLW0 &=~ 1;
-}
